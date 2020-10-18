3b6b55ed1b2cc56dc2ff202ecd34f3b3aaa64426
vim <sidereal@usp.br>
2020-10-18 07:17:42 -0300

    Fixing delete.py

--- src/delete.py
+++ src/delete.py
@@ -7,10 +7,10 @@ def cardapio(update, context):
     keyboard = [[InlineKeyboardButton(text="Clique aqui quando estiver pronto(a) para pedir!", callback_data='help')]]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     prompt = "Este é o nosso cardápio:"
-    photo = open("comandos.png", 'rb')
+    photo = "./static/help_menu.png"
     context.bot.send_message(chat_id=update.effective_chat.id, text=prompt)
     global text_id # Guardando a mensagem com a foto para exclusão
-    text_id = context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=photo, reply_markup=markup)
+    text_id = context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open(photo, 'rb'), reply_markup=markup)
 
 def query_handler(update, context):
     query = update.callback_query
@@ -18,7 +18,7 @@ def query_handler(update, context):
     if query.data == 'help':
         context.bot.edit_message_reply_markup(chat_id=query.message.chat_id, message_id=query.message.message_id) # Deletando o botão
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=text_id.message_id) # Deletando o cardápio verdadeiro
-        media = open("falso.png", 'rb')
-        context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=media) # Enviando o cardápio falso
+        media = "./static/fake_menu.png"
+        context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open(media, 'rb')) # Enviando o cardápio falso
         answer = "Agradecemos pela preferência!"
         context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
