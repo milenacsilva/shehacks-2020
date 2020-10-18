5c48be7b3a9aabbef968bb6b32f265027c60f4c3
vim <sidereal@usp.br>
2020-10-18 08:37:00 -0300

    Optimizing

--- src/menu.py
+++ src/menu.py
@@ -10,7 +10,7 @@ def menu(update, context):
     photo = "./static/help_menu.png"
     context.bot.send_message(chat_id=update.effective_chat.id, text=prompt)
     text_id = context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open(photo, 'rb'), reply_markup=markup)
-    context.bot_data['text_id'] = text_id # Guardando para exclusão posterior da imagem
+    context.bot_data['text_id'] = text_id.message_id # Guardando para exclusão posterior da imagem
 
 def query_handler(update, context):
     query = update.callback_query
@@ -18,7 +18,7 @@ def query_handler(update, context):
     if query.data == 'help':
         text_id = context.bot_data['text_id']
         context.bot.edit_message_reply_markup(chat_id=query.message.chat_id, message_id=query.message.message_id) # Deletando o botão
-        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=text_id.message_id) # Deletando o cardápio verdadeiro
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=text_id) # Deletando o cardápio verdadeiro
         media = "./static/fake_menu.png"
         context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open(media, 'rb')) # Enviando o cardápio falso
         answer = "Agradecemos pela preferência!"
